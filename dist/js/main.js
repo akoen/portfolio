1f9283614cfe2da499c3c3b9e29f677e3b3894dd
Alex Koen <arw.koen@gmail.com>
2019-08-29 15:09:44 -0700

    Updated js

---
+++ dist/js/main.js
@@ -0,0 +1,22 @@
+/*!
+ * project-name v0.0.1
+ * A description for your project.
+ * (c) 2019 YOUR NAME
+ * MIT License
+ * http://link-to-your-git-repo.com
+ */
+
+
+const main = () => {
+}
+
+document.addEventListener('DOMContentLoaded', main)
+
+var rellax = new Rellax('.rellax', {
+    speed: -2,
+    center: false,
+    wrapper: null,
+    round: true,
+    vertical: true,
+    horizontal: false
+});
